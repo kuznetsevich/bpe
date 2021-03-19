8d6cd209e6cc3fe72a3d26f3b89683f7d50a9e49
kuznetsov83 <mr.kuznetsov.vp@gmail.com>
2021-03-19 21:21:36 +0300

    Tasks

--- src/components/Tabs/TasksTab.vue
+++ src/components/Tabs/TasksTab.vue
@@ -1,11 +1,74 @@
 <template>
-  <h1>Task tab</h1>
+  <VContainer class="flex-column">
+    <VLayout column>
+      <VFlex
+          d-flex
+          xs12
+      >
+        <v-expansion-panels>
+
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              Item
+              <template v-slot:actions>
+                <v-icon color="primary">
+                  $expand
+                </v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+          <v-expansion-panel>
+            <v-expansion-panel-header disable-icon-rotate>
+              Item
+              <template v-slot:actions>
+                <v-icon color="teal">
+                  mdi-check
+                </v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+          <v-expansion-panel>
+            <v-expansion-panel-header disable-icon-rotate>
+              Item
+              <template v-slot:actions>
+                <v-icon color="error">
+                  mdi-alert-circle
+                </v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+        </v-expansion-panels>
+      </VFlex>
+    </VLayout>
+  </VContainer>
+
 </template>
 
 <script>
 export default {
-    name: 'TasksTab'
+    name: 'TasksTab',
+  panel: [0, 1],
+  disabled: false,
+  readonly: false,
 };
+
+
+
+
+</script>
+
 </script>
 
 <style scoped>
