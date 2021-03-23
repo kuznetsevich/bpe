85177c59ea03804f8c91787f15bf136fc2b938a2
kuznetsov83 <mr.kuznetsov.vp@gmail.com>
2021-03-23 09:09:36 +0300

    Tasks

--- src/components/Tabs/TasksTab.vue
+++ src/components/Tabs/TasksTab.vue
@@ -13,12 +13,46 @@
               Item
               <template v-slot:actions>
                 <v-icon color="primary">
-                  $expand
+                  mdi-timer-sand
                 </v-icon>
               </template>
             </v-expansion-panel-header>
             <v-expansion-panel-content>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <VLayout column>
+                <VFlex
+                    d-flex
+                    xs12
+                >
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="4"
+                  >
+                    <div class="if-notes">
+                      <h6 class="title">
+                        IF
+                      </h6>
+                      <p class="if-description caption">
+                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                      </p>
+                    </div>
+                  </VCol>
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="4"
+                  >
+                    <div class="if-notes">
+                      <h6 class="title">
+                        IF
+                      </h6>
+                      <p class="if-description caption">
+                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                      </p>
+                    </div>
+                  </VCol>
+                </VFlex>
+              </VLayout>
             </v-expansion-panel-content>
           </v-expansion-panel>
 
