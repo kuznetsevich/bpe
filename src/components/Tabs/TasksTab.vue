291985822682b9fe84d0d6498361cd4c15e1ac19
kuznetsov83 <mr.kuznetsov.vp@gmail.com>
2021-03-25 18:33:15 +0300

    Tasks - statuses

--- src/components/Tabs/TasksTab.vue
+++ src/components/Tabs/TasksTab.vue
@@ -44,7 +44,21 @@
                   >
                     <div class="if-notes">
                       <h6 class="title">
-                        IF
+                        AND
+                      </h6>
+                      <p class="if-description caption">
+                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                      </p>
+                    </div>
+                  </VCol>
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="4"
+                  >
+                    <div class="if-notes">
+                      <h6 class="title">
+                        THEN
                       </h6>
                       <p class="if-description caption">
                         Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
@@ -53,7 +67,103 @@
                   </VCol>
                 </VFlex>
                 <v-divider
-                  class="pa-2"
+                    class="pa-2"
+                ></v-divider>
+                <VFlex
+                    align-baseline
+                    d-flex
+                    xs12
+                >
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="6"
+                  >
+                    <v-btn
+                        disabled
+                        color="primary"
+                    >
+                      Download CSV
+                    </v-btn>
+                  </VCol>
+                  <VCol
+                      class="d-flex task-restore-btn"
+                      cols="12"
+                      sm="6"
+                  >
+                    <p class="subheading">Processing...</p>
+                  </VCol>
+                </VFlex>
+                <v-progress-linear
+                    color="deep-purple accent-4"
+                    indeterminate
+                    rounded
+                    height="6"
+                ></v-progress-linear>
+              </VLayout>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+
+          <v-expansion-panel>
+            <v-expansion-panel-header disable-icon-rotate>
+              Item
+              <template v-slot:actions>
+                <v-icon color="teal">
+                  mdi-check
+                </v-icon>
+              </template>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <VLayout column>
+                <VFlex
+                    d-flex
+                    xs12
+                >
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="4"
+                  >
+                    <div class="if-notes">
+                      <h6 class="title">
+                        IF
+                      </h6>
+                      <p class="if-description caption">
+                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                      </p>
+                    </div>
+                  </VCol>
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="4"
+                  >
+                    <div class="if-notes">
+                      <h6 class="title">
+                        AND
+                      </h6>
+                      <p class="if-description caption">
+                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                      </p>
+                    </div>
+                  </VCol>
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="4"
+                  >
+                    <div class="if-notes">
+                      <h6 class="title">
+                        THEN
+                      </h6>
+                      <p class="if-description caption">
+                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                      </p>
+                    </div>
+                  </VCol>
+                </VFlex>
+                <v-divider
+                    class="pa-2"
                 ></v-divider>
                 <VFlex
                     d-flex
@@ -92,27 +202,93 @@
             <v-expansion-panel-header disable-icon-rotate>
               Item
               <template v-slot:actions>
-                <v-icon color="teal">
-                  mdi-check
-                </v-icon>
-              </template>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-
-          <v-expansion-panel>
-            <v-expansion-panel-header disable-icon-rotate>
-              Item
-              <template v-slot:actions>
                 <v-icon color="error">
                   mdi-alert-circle
                 </v-icon>
               </template>
             </v-expansion-panel-header>
             <v-expansion-panel-content>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <VLayout column>
+                <VFlex
+                    d-flex
+                    xs12
+                >
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="4"
+                  >
+                    <div class="if-notes">
+                      <h6 class="title">
+                        IF
+                      </h6>
+                      <p class="if-description caption">
+                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                      </p>
+                    </div>
+                  </VCol>
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="4"
+                  >
+                    <div class="if-notes">
+                      <h6 class="title">
+                        AND
+                      </h6>
+                      <p class="if-description caption">
+                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                      </p>
+                    </div>
+                  </VCol>
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="4"
+                  >
+                    <div class="if-notes">
+                      <h6 class="title">
+                        THEN
+                      </h6>
+                      <p class="if-description caption">
+                        Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+                      </p>
+                    </div>
+                  </VCol>
+                </VFlex>
+                <v-divider
+                    class="pa-2"
+                ></v-divider>
+                <VFlex
+                    d-flex
+                    xs12
+                >
+                  <VCol
+                      class="d-flex"
+                      cols="12"
+                      sm="6"
+                  >
+                    <v-btn
+                        disabled
+                        color="primary"
+                    >
+                      Download CSV
+                    </v-btn>
+                  </VCol>
+                  <VCol
+                      class="d-flex task-restore-btn"
+                      cols="12"
+                      sm="6"
+                  >
+                    <v-btn
+                        depressed
+                        color="error"
+                    >
+                      Push
+                    </v-btn>
+                  </VCol>
+                </VFlex>
+              </VLayout>
             </v-expansion-panel-content>
           </v-expansion-panel>
 
