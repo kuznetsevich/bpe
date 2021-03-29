43d8b36a55144ef3c88f7b789a1cf192d777f894
kuznetsov83 <mr.kuznetsov.vp@gmail.com>
2021-03-29 17:07:44 +0300

    privacy policy bar

--- src/components/Tabs/IndexTab.vue
+++ src/components/Tabs/IndexTab.vue
@@ -298,6 +298,14 @@
         </VCol>
       </VFlex>
     </VLayout>
+
+    <v-alert
+        class="mx-4"
+        dense
+        type="info"
+    >
+      I'm a dense alert with a <strong>type</strong> of info
+    </v-alert>
   </VContainer>
 </template>
 
